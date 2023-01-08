1434edd6777d37462e290715023383905d6e1513
Irene Aunon <irene@ireneaunion.com>
2023-01-08 22:11:40 +0100

    remove unused dependencies

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -20,7 +19,6 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
